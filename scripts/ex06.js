1193ef21910746bb0abeef78a52aee10c82817c4
natan_v_barbosa <natan_v_barbosa@estudante.sesisenai.org.br>
2026-03-25 12:21:47 -0300

    add- Finalização da 6°

---
+++ scripts/ex06.js
@@ -0,0 +1,11 @@
+function vogalConsoante(){
+    let Vogal = String;
+    let Consoante = String;
+    let letra = prompt("Informe uma letra");
+    if (letra === "a"|| letra ==="e"|| letra ==="i"|| letra ==="o"|| letra ==="u"){
+        alert("Isso é uma vogal");
+    }else{
+        alert("Isso é uma consoante");
+    }
+    
+}
