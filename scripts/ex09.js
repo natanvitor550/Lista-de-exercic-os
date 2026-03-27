a08c656f135bbaca2bd3bd26dbab24f27701581f
natan_v_barbosa <natan_v_barbosa@estudante.sesisenai.org.br>
2026-03-27 11:41:35 -0300

    add- Finalização da 9°

---
+++ scripts/ex09.js
@@ -0,0 +1,26 @@
+function Ordenacao_2(){
+    const numeros = [];
+    for (let i=0; i<2; i++){
+        let numero = parseFloat(prompt("Informe um número: "));
+        if (!isNaN(numero)){
+            numeros.push(numero);
+        }else{
+            alert("Digite um número válido!");
+            i--;
+        }
+    }
+    function insertionSort(vetor) {
+    for (let i = 1; i < vetor.length; i++) {
+        let chave = vetor[i];
+        let j = i - 1;
+        while (j >= 0 && vetor[j] < chave) { 
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        vetor[j + 1] = chave;
+    }
+    return vetor;
+}
+    const numerosOrdenados = insertionSort(numeros);
+    alert("A ordem decrescente é: " + numerosOrdenados);
+}
