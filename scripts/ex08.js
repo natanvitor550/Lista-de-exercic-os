302b446aa6628f88d2166d6e00147481550bf660
natan_v_barbosa <natan_v_barbosa@estudante.sesisenai.org.br>
2026-03-27 11:37:27 -0300

    add- Finalização da 8°

---
+++ scripts/ex08.js
@@ -0,0 +1,7 @@
+function Operacao(){
+    let Numero1 = Math.floor(Math.random() * 100);
+    let Numero2 = Math.floor(Math.random() * 100);
+    alert("A diferença entre as variáveis é: " + (Numero1 - Numero2));
+    alert("A soma do dobro do primeiro número e o triplo do segundo é: " + (Numero1 * 2) + (Numero2 * 3));
+    alert("A multiplicação dos dois números é: " + (Numero1 * Numero2));
+}
